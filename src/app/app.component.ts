9c0080bd8dac6b4711c6688613f5a2eef7fb81c7
Hadi Roshan <HadiRoshanNDS@gmail.com>
2025-04-14 13:12:53 +0330

    Added the filter for fulfilled or not fulfilled, April 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { WishItem } from './shared/models/wishItem';
 
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete
+]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +17,28 @@ export class AppComponent {
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
+
+  listFilter: any = '0';
+
+  newWishText = '';
+
   title = 'wish list';
+
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+    // let value = this.listFilter;
+    // if(value === '0') {
+    //   return this.items;
+    // } else if (value === '1') {
+    //   return this.items.filter(item => !item.isComplete)
+    // } else {
+    //   return this.items.filter(item => item.isComplete)
+    };
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
