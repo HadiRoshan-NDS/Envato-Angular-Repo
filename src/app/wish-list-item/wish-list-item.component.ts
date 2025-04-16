151e47f7d17964cfb12ebcf922d7530456e8b997
Hadi Roshan <HadiRoshanNDS@gmail.com>
2025-04-16 11:06:46 +0330

    Refactoring the WishListItem component and implementing the functionality for removing items from the list

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -8,22 +8,21 @@ import events from '../shared/services/eventService'
   styleUrls: ['./wish-list-item.component.scss']
 })
 export class WishListItemComponent {
-  @Input() wishText!: string;
-  @Input() fulfilled!: boolean;
-  @Output() fulfilledChange = new EventEmitter<boolean>();
+  @Input() wish!: WishItem;
+  // @Input() fulfilled!: boolean;
+  // @Output() fulfilledChange = new EventEmitter<boolean>();
 
   get cssClasses() {
     //return this.fulfilled ? ['strikeout', 'text-muted'] : [];
-    return {'strikeout text-muted': this.fulfilled};
+    return {'strikeout text-muted': this.wish.isComplete};
   }
 
   removeWish() {
-    events.emit('removeWish', this.wishText)
+    events.emit('removeWish', this.wish)
   }
 
   toggleFulfilled() {
-    this.fulfilled = !this.fulfilled;
-    this.fulfilledChange.emit(this.fulfilled);
+this.wish.isComplete = !this.wish.isComplete;
   }
 
 }
